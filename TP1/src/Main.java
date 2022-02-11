2b43d6a0dc13022e0d935a7def838970e9daa8d4
Benoit <benoit.thomas@eleve.isep.fr>
2022-02-11 16:32:49 +0100

    tp1 finito

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -2,19 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        init();
+        volume();
     }
 
     public static void init() { //fonction initiale
         Scanner scanner = new Scanner(System.in); //récupérer les entrées consoles
+        System.out.println("Bonjour, quel est votre prénom ?");
+        String prenom = scanner.nextLine();
+        System.out.println("Bonjour, " + prenom);
         int unEntier = scanner.nextInt(); //nombre 1
         float unReel = scanner.nextFloat(); //nombre 2
         System.out.println("J’ai recupere un entier: " + unEntier); //affichage de l'entier'
         System.out.println("J’ai aussi recupere un reel: " + unReel); //affichage du réel
-        System.out.println("Bonjour, quel est votre prénom ?\n");
-
-        String prenom = scanner.nextLine();
-        System.out.println("Bonjour, " + prenom);
     }
 
     public static void somme(){
@@ -24,9 +23,9 @@ public class Main {
         System.out.println("Veuillez saisir le deuxieme entier");
         int deuxiemeEntier = scanner.nextInt();
         int somme = premierEntier + deuxiemeEntier;
-        System.out.println("La somme de " + premierEntier + " avec " + deuxiemeEntier +
-                "est egale a " + somme);
+        System.out.println("La somme de " + premierEntier + " avec " + deuxiemeEntier + " est egale a " + somme);
     }
+
     public static void division(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Veuillez saisir le premier entier");
@@ -34,11 +33,19 @@ public class Main {
         System.out.println("Veuillez saisir le deuxieme entier");
         int deuxiemeEntier = scanner.nextInt();
         int division = premierEntier / deuxiemeEntier;
-        System.out.println("La division de " + premierEntier + " avec " + deuxiemeEntier +
-                "est egale a " + division);
+        System.out.println("La division de " + premierEntier + " avec " + deuxiemeEntier + " est egale a " + division);
     }
-    public static void volume(){
 
+    public static void volume(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez saisir le premier entier");
+        float premierEntier = scanner.nextInt();
+        System.out.println("Veuillez saisir le deuxieme entier");
+        float deuxiemeEntier = scanner.nextInt();
+        System.out.println("Veuillez saisir le troisieme entier");
+        float troisiemeEntier = scanner.nextInt();
+        float volume = premierEntier * deuxiemeEntier * troisiemeEntier;
+        System.out.println("Le volume est de " + volume + " insérer unité");
     }
 
 }
