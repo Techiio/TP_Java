fe2546144d83edfb6783907065c4ccb074dbdf8a
Benoit <benoit.thomas@eleve.isep.fr>
2022-04-01 16:24:34 +0200

    RPG: modif javafx

--- RPG/src/main/java/com/example/rpg/JavaFX.java
+++ RPG/src/main/java/com/example/rpg/JavaFX.java
@@ -15,13 +15,16 @@ public class JavaFX {
     }
 
     public void start(Stage stage) throws IOException {
+        int heroAleatoire = 0 + (int)(Math.random() * ((4 - 0) + 1));
+        int enemyAleatoire = 0 + (int)(Math.random() * ((2 - 0) + 1));
+
         GridPane gridPane = new GridPane();
-        Button btn = CustomButtonFactory.createButton("Pick hero 1", game.getHeroes()[0].toString());
-        Button btn2 = CustomButtonFactory.createButton("Pick enemy 1", game.getEnemies()[0].toString());
+        Button btn = CustomButtonFactory.createButton("Pick hero", game.getHeroes()[heroAleatoire].toString());
+        Button btn2 = CustomButtonFactory.createButton("Pick enemy", game.getEnemies()[enemyAleatoire].toString());
         gridPane.add(btn, 10, 10);
         gridPane.add(btn2, 10, 20);
         Scene scene = new Scene(gridPane, 600, 400);
-        stage.setTitle("RPG");
+        stage.setTitle("Elden Ring II (enkor + dur)");
         stage.setScene(scene);
         stage.show();
     }
